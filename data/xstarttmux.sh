308e1453ade6a5531f677cc37c3e6768b62eba02
ihlec <ihle.cornelius@gmail.com>
2020-05-09 12:30:33 +0200

    tmux script comments

--- data/xstarttmux.sh
+++ data/xstarttmux.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # Input number expected e.g. "./xstarttmux.sh 3"
+
 # tmux kill-server
 # tmux list-sessions
 # tmux attach -t n
+
+# Expects binary in same folder or in bin path
 
 for (( i = 0; i <= $1; i++)) do tmux new-session -d -s $i && tmux send ./bc_p2p Enter && echo "$i Started"; done
 
