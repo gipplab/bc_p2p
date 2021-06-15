6f12655093111d3595fe12837a7e17cf04bb5962
Cornelius Ihle <ihle.cornelius@gmail.com>
2021-06-15 16:10:54 +0200

    run script

--- src/go/bc_dht/runPlan.sh
+++ src/go/bc_dht/runPlan.sh
@@ -1,5 +1,1 @@
-Jun 15 13:28:55.039917	[34mINFO[0m	created a synthetic composition file for this job; all instances will run under singleton group "single"
-Jun 15 13:28:55.039958	[34mINFO[0m	using home directory: /home/manjaro/testground
-Jun 15 13:28:55.039999	[34mINFO[0m	no .env.toml found at /home/manjaro/testground/.env.toml; running with defaults
-Jun 15 13:28:55.040005	[34mINFO[0m	testground client initialized	{"addr": "localhost:8042"}
-failed to resolve test plan: failed to locate plan in directory: /home/manjaro/testground/plans/quickstart
+testground run single --plan=bc_test --testcase=quickstart --runner=local:docker --builder=docker:go --instances=1
