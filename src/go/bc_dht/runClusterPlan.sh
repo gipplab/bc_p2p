a98b401da13f61312ecbdaba09cc9c597126476f
Cornelius Ihle <ihle.cornelius@gmail.com>
2021-08-03 14:31:27 +0200

    K8s test

--- src/go/bc_dht/runClusterPlan.sh
+++ src/go/bc_dht/runClusterPlan.sh
@@ -1,1 +1,1 @@
-testground run single --plan=bc_test --testcase=quickstart --runner=cluster:k8s --builder=docker:go --instances=1
+testground run single --plan=coop_bc --testcase=bc --runner=cluster:k8s --builder=docker:go --instances=1
