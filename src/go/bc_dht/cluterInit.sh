280ebdbed530e1ee7cf14a2a1775c28ed0748d8d
Cornelius Ihle <ihle.cornelius@gmail.com>
2021-08-11 09:11:47 +0200

    kops cluster

---
+++ src/go/bc_dht/cluterInit.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/testground/infra
+
+cd infra
+
+./k8s/install.sh ./k8s/cluster.yaml
---
+++ src/go/bc_dht/helms.conf.sh
@@ -0,0 +1,4 @@
+helm repo add stable https://charts.helm.sh/stable
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add influxdata https://helm.influxdata.com/
+helm repo update
