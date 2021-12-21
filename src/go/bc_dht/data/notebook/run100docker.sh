646ed661512f244f6d1225c9bc1ca3c04d420405
Cornelius Ihle <ihle.cornelius@gmail.com>
2021-12-21 08:31:16 +0100

    Repeating Runs skript

--- src/go/bc_dht/data/notebook/run100docker.sh
+++ src/go/bc_dht/data/notebook/run100docker.sh
@@ -9,23 +9,29 @@ echo ""
 
 # $1 max number of peers
 # $2 increase step per run
+# $3 number of repetitions
 if [ $# -eq 0 ]
   then
-    echo "No arguments supplied. 1: max number of peers; 2: increase step per run"
+    echo "No arguments supplied. 1: max number of peers; 2: increase step per run; 3: number of repetitions"
     exit 1
 fi
 
 pushd ../outputs/local_docker
 
-# Runs
-for (( i = 0; i <= $1; i = i+$2)) do
-    if [ $i == 0 ]; then 
-        continue
-    fi
-    echo "Run with $i peers. Results at: "
-    pwd
-    testground run single --plan=coopbc --testcase=bc --runner=local:docker --builder=docker:go --instances=$i --collect
-    sleep 60s 
-done
+# Repetitions
+for (( r = 1; r <= $3; r++)) do
+    echo "Repetition $r of $3: "
 
+    # Runs
+    for (( i = 0; i <= $1; i = i+$2)) do
+        if [ $i == 0 ]; then 
+            continue
+        fi
+        echo "Run with $i peers. Results at: "
+        pwd
+        testground run single --plan=coopbc --testcase=bc --runner=local:docker --builder=docker:go --instances=$i --collect
+        sleep 90s
+    done
+    sleep 60s
+done
 popd 
